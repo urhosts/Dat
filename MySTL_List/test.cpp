1411c6f57d73eadb319ff410b57a2a9444dd3f5e
urhosts <hosts@gsxio.com>
2017-05-18 20:01:42 +0800

    MySTL_List

---
+++ MySTL_List/test.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: hosts
+	> Mail: hosts@gsxio.com
+	> Created Time: Thu 18 May 2017 10:22:54 AM CST
+ ************************************************************************/
+
+#include <iostream>
+#include <vector>
+#include "MyList.h"
+
+using namespace std;
+
+int main()
+{
+    test();
+    return 0;
+}
