63fd02a922e88a24ef201d18a5ce4dfa75dafa66
urhosts <hosts@gsxio.com>
2017-06-03 23:54:03 +0800

    Heap

---
+++ Heap/main.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: hosts
+	> Mail: hosts@gsxio.com
+	> Created Time: Sat 03 Jun 2017 11:28:42 PM CST
+ ************************************************************************/
+
+#include "Heap.h"
+using namespace std;
+
+int main()
+{
+    TestHeap();
+    return 0;
+}
